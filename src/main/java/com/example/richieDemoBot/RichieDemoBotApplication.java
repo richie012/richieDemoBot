f4d4e605b294bfe702dab1345133361abf959b51
richie <82734398+richie012@users.noreply.github.com>
2022-12-01 22:02:32 +0500

    Add files via upload

---
+++ src/main/java/com/example/richieDemoBot/RichieDemoBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.richieDemoBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichieDemoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichieDemoBotApplication.class, args);
+	}
+
+}
